7aa28ef60d9dd3dc1f35689a183601e6ddb94e2e
Watson Rogers <watjrogers09@gmail.com>
2026-07-04 16:28:58 -0500

    Basic transfer of data from API to visible page

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import os
+
+import requests
+
+load_dotenv()
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+city = "Paris"
 
 # Create your views here.
+def home(request):
+
+    url = f'https://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={city}'
+    response = requests.get(url)
+    data = response.json()
+
+    current = data['current'].items()
+    
+    context = {
+        'current' : current
+    }
+
+    return render(request, 'weather_app/home.html', context)
